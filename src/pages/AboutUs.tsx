4e2cfa74353e0337b1414d11a569556b66b681ca
Aliaksandr Bulynka <abulynka@gmail.com>
2023-03-17 18:30:18 +0100

    feat: add about page

---
+++ src/pages/AboutUs.tsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './AboutUs.css';
+
+class AboutUs extends Component {
+  render() {
+    return <main className="AboutUs">About Us Page</main>;
+  }
+}
+
+export default AboutUs;
