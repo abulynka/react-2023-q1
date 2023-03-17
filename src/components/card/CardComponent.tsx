e292ecdb0fc4046284564116cff76810e8023696
Aliaksandr Bulynka <abulynka@gmail.com>
2023-03-17 18:26:38 +0100

    feat: add cards data

---
+++ src/components/card/CardComponent.tsx
@@ -0,0 +1,42 @@
+import './CardComponent.css';
+import React, { Component } from 'react';
+import CardItem from '../../data/CardItem';
+import { Card, CardHeader, CardMedia, CardContent, Typography, CardActions } from '@mui/material';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import VisibilityIcon from '@mui/icons-material/Visibility';
+
+class CardComponent extends Component<{ card: CardItem }> {
+  public render(): JSX.Element {
+    return (
+      <Card className="card" sx={{ maxWidth: 345 }}>
+        <CardHeader className="card__title" title={this.props.card.title} />
+        <CardMedia
+          className="card__image"
+          component="img"
+          height="194"
+          image={this.props.card.imageURL}
+          alt={this.props.card.title}
+        />
+        <CardContent>
+          <Typography variant="body2" color="text.secondary">
+            {this.props.card.desc}
+          </Typography>
+        </CardContent>
+
+        <CardActions className="card__container" disableSpacing>
+          <div className="card__container-likes">
+            <FavoriteIcon></FavoriteIcon>
+            {this.props.card.likes}
+          </div>
+
+          <div className="card__container-views">
+            <VisibilityIcon></VisibilityIcon>
+            {this.props.card.views}
+          </div>
+        </CardActions>
+      </Card>
+    );
+  }
+}
+
+export default CardComponent;
