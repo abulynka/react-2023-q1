651f0da39ccbe822f0697cf148f1218359749ebf
Aliaksandr Bulynka <abulynka@gmail.com>
2023-03-17 18:30:27 +0100

    feat: add main page

---
+++ src/pages/Main.tsx
@@ -0,0 +1,25 @@
+import './Main.css';
+import React, { Component } from 'react';
+import Search from '../components/search/Search';
+import CardModel from '../models/CardModel';
+import CardComponent from '../components/card/CardComponent';
+import CardItem from '../data/CardItem';
+
+class Main extends Component {
+  public render() {
+    return (
+      <main className="main">
+        <div className="main__search">
+          <Search />
+        </div>
+        <section className="main__cards">
+          {new CardModel().getCards().map((card: CardItem) => (
+            <CardComponent key={card.number} card={card} />
+          ))}
+        </section>
+      </main>
+    );
+  }
+}
+
+export default Main;
