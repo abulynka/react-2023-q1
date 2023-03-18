9bb2c6b118166a31181a018d872dc4e1fcc7cb2b
Aliaksandr Bulynka <abulynka@gmail.com>
2023-03-18 23:00:43 +0100

    feat: add set/get state and set/get localStorage for Search component

--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -1,19 +1,33 @@
 import './Search.css';
-import React, { Component } from 'react';
+import React, { ChangeEvent, Component } from 'react';
 import { Button, TextField } from '@mui/material';
 
-class Search extends Component {
+class Search extends Component<object, { input: string }> {
+  constructor(props: object) {
+    super(props);
+    this.state = { input: localStorage.getItem('search__text') || '' };
+  }
+
   public render(): JSX.Element {
     return (
       <div className="search">
         <div className="search__text">
-          <TextField />
+          <TextField
+            defaultValue={this.state.input}
+            onChange={(value: ChangeEvent<HTMLInputElement>) =>
+              this.setState({ input: value.target.value })
+            }
+          />
         </div>
         <div className="search__button">
           <Button variant="contained">Search</Button>
         </div>
       </div>
     );
+  }
+
+  public componentWillUnmount(): void {
+    localStorage.setItem('search__text', this.state.input);
   }
 }
 
