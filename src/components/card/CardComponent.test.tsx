41ae1536d47f9d699ae645f922a8bd27f767d647
Aliaksandr Bulynka <abulynka@gmail.com>
2023-03-19 15:13:23 +0100

    feat: add CardComponent test

---
+++ src/components/card/CardComponent.test.tsx
@@ -0,0 +1,21 @@
+import { describe, expect, it } from 'vitest';
+import { render, screen } from '@testing-library/react';
+
+import CardComponent from './CardComponent';
+import CardItem from '../../data/CardItem';
+import React from 'react';
+
+describe('CardComponent', () => {
+  it('Hello, World!', () => {
+    const cardItem: CardItem = {
+      number: '0',
+      imageURL: '/assets/cabbage.png',
+      title: 'cabbage',
+      desc: 'tasty cabbage',
+      likes: 10,
+      views: 1000,
+    };
+    render(<CardComponent card={cardItem} />);
+    expect(screen.getByRole('heading')).toHaveTextContent('cabbage');
+  });
+});
