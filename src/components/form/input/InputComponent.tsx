6e7620e6da842f3e8013b0c23d6d5cb9254e2de5
Aliaksandr Bulynka <abulynka@gmail.com>
2023-03-26 21:03:52 +0200

    feat: add input component

---
+++ src/components/form/input/InputComponent.tsx
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { FormInputProps } from '../FormInputProps';
+import ErrorMessageComponent from '../ErrorMessageComponent';
+import './InputComponent.css';
+
+class InputComponent extends Component<FormInputProps> {
+  public render() {
+    return (
+      <div className="input">
+        <label className="input__label">
+          {this.props.name}:
+          <input type="string" className="input__control" ref={this.props.refObject}></input>
+        </label>
+
+        {this.props.errorMessages.map((errorMessage: string) => (
+          <ErrorMessageComponent
+            key={crypto.randomUUID()}
+            errorMessage={errorMessage}
+          ></ErrorMessageComponent>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default InputComponent;
