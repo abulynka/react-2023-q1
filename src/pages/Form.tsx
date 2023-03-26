0bb19b8dbc84eb4b5d30b29b4f3d2652957823d0
Aliaksandr Bulynka <abulynka@gmail.com>
2023-03-26 21:07:19 +0200

    feat: change form to components

--- src/pages/Form.tsx
+++ src/pages/Form.tsx
@@ -1,24 +1,62 @@
-import React, { Component } from 'react';
+import React, { Component, createRef } from 'react';
 import './Form.css';
 import FormCardListComponent from '../components/form/FormCardListComponent';
 import FormItem from '../data/FormItem';
+import InputComponent from '../components/form/input/InputComponent';
+import { ErrorMessages } from '../components/form/ErrorMessages';
+import InputDateComponent from '../components/form/inputDate/InputDateComponent';
+import SelectComponent from '../components/form/select/SelectComponent';
+import InputCheckboxComponent from '../components/form/inputCheckbox/InputCheckboxComponent';
+import InputRadioYNComponent from '../components/form/inputRadioYN/InputRadioYNComponent';
+import InputImageComponent from '../components/form/inputImage/InputImageComponent';
 
 interface FormItemElement {
-  name: HTMLInputElement;
-  bestBeforeDate: HTMLInputElement;
+  base: React.RefObject<HTMLFormElement>;
 
-  deliveryBy: HTMLSelectElement;
+  name: React.RefObject<HTMLInputElement>;
 
-  cook: HTMLInputElement;
+  bestBeforeDate: React.RefObject<HTMLInputElement>;
 
-  shouldBePackedYes: HTMLInputElement;
+  deliveryBy: React.RefObject<HTMLSelectElement>;
 
-  shouldBePackedNo: HTMLInputElement;
+  cook: React.RefObject<HTMLInputElement>;
 
-  file: HTMLInputElement;
+  shouldBePackedYes: React.RefObject<HTMLInputElement>;
+
+  shouldBePackedNo: React.RefObject<HTMLInputElement>;
+
+  file: React.RefObject<HTMLInputElement>;
 }
 
-class Form extends Component<object, Record<string, FormItem[]>> {
+interface FormState {
+  formItems: FormItem[];
+
+  name: {
+    errorMessages: string[];
+  };
+
+  bestBeforeDate: {
+    errorMessages: string[];
+  };
+
+  deliveryBy: {
+    errorMessages: string[];
+  };
+
+  cook: {
+    errorMessages: string[];
+  };
+
+  shouldBePacked: {
+    errorMessages: string[];
+  };
+
+  file: {
+    errorMessages: string[];
+  };
+}
+
+class Form extends Component<object, FormState> {
   private formItem: FormItemElement = {} as FormItemElement;
 
   public constructor(props: Record<string, string[]>) {
@@ -26,127 +64,188 @@ class Form extends Component<object, Record<string, FormItem[]>> {
 
     this.state = {
       formItems: [],
+      name: {
+        errorMessages: [],
+      },
+      bestBeforeDate: {
+        errorMessages: [],
+      },
+      deliveryBy: {
+        errorMessages: [],
+      },
+      cook: {
+        errorMessages: [],
+      },
+      shouldBePacked: {
+        errorMessages: [],
+      },
+      file: {
+        errorMessages: [],
+      },
     };
+  }
+
+  private validateRequired(value: string, messages: string[]) {
+    if (value === '') {
+      messages.push(ErrorMessages.REQUIRED);
+    }
+  }
+
+  private validateName(value: string): string[] {
+    const messages: string[] = [];
+
+    this.validateRequired(value, messages);
+
+    return messages;
+  }
+
+  private validatebestBeforeDate(value: string): string[] {
+    const messages: string[] = [];
+
+    this.validateRequired(value, messages);
+
+    return messages;
+  }
+
+  private validateDeliveryBy(value: string): string[] {
+    const messages: string[] = [];
+
+    return messages;
+  }
+
+  private validateCook(value: boolean | null) {
+    const messages: string[] = [];
+
+    return messages;
+  }
+
+  private validateShouldBePacked(valueYes: boolean, valueNo: boolean): string[] {
+    const messages: string[] = [];
+
+    if (!valueYes && !valueNo) {
+      messages.push(ErrorMessages.REQUIRED);
+    }
+
+    return messages;
+  }
+
+  private validateFile(value: string): string[] {
+    const messages: string[] = [];
+
+    this.validateRequired(value, messages);
+
+    return messages;
   }
 
   public submit = (e: React.FormEvent) => {
     e.preventDefault();
 
-    // const formItem: FormItem = {} as FormItem;
-    // formItem.name = this.formItem.name.value;
-    // formItem.bestBeforeDate = this.formItem.bestBeforeDate.value;
-    // formItem.deliveryBy = this.formItem.deliveryBy.value;
-    // formItem.cook = this.formItem.cook.checked;
-    // formItem.shouldBePacked = this.formItem.shouldBePackedYes.checked;
-    // formItem.file = this.formItem.file.value;
+    const formItem: FormItem = {} as FormItem;
+    formItem.name = this.formItem.name.current?.value || '';
+    formItem.bestBeforeDate = this.formItem.bestBeforeDate.current?.value || '';
+    formItem.deliveryBy = this.formItem.deliveryBy.current?.value || '';
+    formItem.cook = this.formItem.cook.current?.checked || null;
+    formItem.shouldBePackedYes = this.formItem.shouldBePackedYes.current?.checked || false;
+    formItem.shouldBePackedNo = this.formItem.shouldBePackedNo.current?.checked || false;
+    formItem.file = this.formItem.file.current?.value || '';
+    formItem.files = this.formItem.file.current?.files;
 
-    const formItem: FormItem = {
-      bestBeforeDate: '2023-03-21',
-      cook: true,
-      deliveryBy: 'Tomorrow',
-      file: 'C:\fakepath\batman.png',
-      name: 'Test',
-      shouldBePacked: true,
+    const errorMessages = {
+      name: {
+        errorMessages: this.validateName(formItem.name),
+      },
+      bestBeforeDate: {
+        errorMessages: this.validatebestBeforeDate(formItem.bestBeforeDate),
+      },
+      deliveryBy: {
+        errorMessages: this.validateDeliveryBy(formItem.deliveryBy),
+      },
+      cook: {
+        errorMessages: this.validateCook(formItem.cook),
+      },
+      shouldBePacked: {
+        errorMessages: this.validateShouldBePacked(
+          formItem.shouldBePackedYes,
+          formItem.shouldBePackedNo
+        ),
+      },
+      file: {
+        errorMessages: this.validateFile(formItem.file),
+      },
     };
 
+    const formItems: FormItem[] = this.state.formItems;
+    if (
+      errorMessages.name.errorMessages.length === 0 &&
+      errorMessages.bestBeforeDate.errorMessages.length === 0 &&
+      errorMessages.deliveryBy.errorMessages.length === 0 &&
+      errorMessages.cook.errorMessages.length === 0 &&
+      errorMessages.shouldBePacked.errorMessages.length === 0 &&
+      errorMessages.file.errorMessages.length === 0
+    ) {
+      formItems.push(formItem);
+      this.formItem.base.current?.reset();
+    }
+
     this.setState({
-      formItems: [formItem, ...this.state.formItems],
+      formItems: formItems,
+      ...errorMessages,
     });
   };
 
   public render(): JSX.Element {
+    this.formItem.base = createRef<HTMLFormElement>();
+
+    this.formItem.name = createRef<HTMLInputElement>();
+    this.formItem.bestBeforeDate = createRef<HTMLInputElement>();
+    this.formItem.deliveryBy = createRef<HTMLSelectElement>();
+    this.formItem.cook = createRef<HTMLInputElement>();
+    this.formItem.shouldBePackedYes = createRef<HTMLInputElement>();
+    this.formItem.shouldBePackedNo = createRef<HTMLInputElement>();
+    this.formItem.file = createRef<HTMLInputElement>();
+
     return (
       <div className="form">
-        <form className="form__container" onSubmit={this.submit}>
-          <label className="form__item">
-            Name:
-            <input
-              type="string"
-              className="form__item-control"
-              ref={(input: HTMLInputElement) => {
-                this.formItem.name = input;
-              }}
-            ></input>
-          </label>
+        <form className="form__container" onSubmit={this.submit} ref={this.formItem.base}>
+          <InputComponent
+            refObject={this.formItem.name}
+            name={'Name'}
+            errorMessages={this.state.name.errorMessages}
+          ></InputComponent>
 
-          <label className="form__item">
-            Best Before Date:
-            <input
-              type="date"
-              className="form__item-control"
-              ref={(input: HTMLInputElement) => {
-                this.formItem.bestBeforeDate = input;
-              }}
-            ></input>
-          </label>
+          <InputDateComponent
+            refObject={this.formItem.bestBeforeDate}
+            name="Best Before Date"
+            errorMessages={this.state.bestBeforeDate.errorMessages}
+          ></InputDateComponent>
 
-          <label className="form__item">
-            Delivery by:
-            <select
-              className="form__item-control"
-              ref={(input: HTMLSelectElement) => {
-                this.formItem.deliveryBy = input;
-              }}
-            >
-              <option>Today</option>
-              <option>Tomorrow</option>
-              <option>After Tomorrow</option>
-            </select>
-          </label>
+          <SelectComponent
+            refObject={this.formItem.deliveryBy}
+            name={'Delivery by'}
+            options={['Today', 'Tomorrow', 'After Tomorrow']}
+            errorMessages={this.state.deliveryBy.errorMessages}
+          ></SelectComponent>
 
-          <fieldset className="form__item">
-            <legend>Choose extra feature:</legend>
-            <div>
-              <input
-                type="checkbox"
-                id="cook"
-                name="cook"
-                ref={(input: HTMLInputElement) => {
-                  this.formItem.cook = input;
-                }}
-              />
-              <label htmlFor="cook">Cook</label>
-            </div>
-          </fieldset>
+          <InputCheckboxComponent
+            refObject={this.formItem.cook}
+            name={'Cook'}
+            title={'Choose extra feature'}
+            errorMessages={this.state.cook.errorMessages}
+          ></InputCheckboxComponent>
 
-          <fieldset className="form__item">
-            <legend>Should be packed:</legend>
-            <div>
-              <input
-                type="radio"
-                id="packed-yes"
-                name="packed"
-                value="packed-yes"
-                ref={(input: HTMLInputElement) => {
-                  this.formItem.shouldBePackedYes = input;
-                }}
-              ></input>
-              <label htmlFor="packed-yes">Yes</label>
-            </div>
-            <div>
-              <input
-                type="radio"
-                id="packed-no"
-                name="packed"
-                value="packed-no"
-                ref={(input: HTMLInputElement) => {
-                  this.formItem.shouldBePackedNo = input;
-                }}
-              ></input>
-              <label htmlFor="packed-no">No</label>
-            </div>
-          </fieldset>
+          <InputRadioYNComponent
+            refObjectYes={this.formItem.shouldBePackedYes}
+            refObjectNo={this.formItem.shouldBePackedNo}
+            name={'packed'}
+            title={'Should be packed'}
+            errorMessages={this.state.shouldBePacked.errorMessages}
+          ></InputRadioYNComponent>
 
-          <label className="form__item">
-            File:
-            <input
-              type="file"
-              accept="image/*"
-              ref={(input: HTMLInputElement) => {
-                this.formItem.file = input;
-              }}
-            ></input>
-          </label>
+          <InputImageComponent
+            refObject={this.formItem.file}
+            name={'File'}
+            errorMessages={this.state.file.errorMessages}
+          ></InputImageComponent>
 
           <div className="form__item">
             <input type="submit" value="Submit" />
