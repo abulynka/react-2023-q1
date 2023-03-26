24d50a50ed6e0321dceabcff2829d06058dd5a9a
Aliaksandr Bulynka <abulynka@gmail.com>
2023-03-26 13:00:26 +0200

    feat: add form route

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Route, Routes } from 'react-router-dom';
 import Main from './pages/Main';
 import AboutUs from './pages/AboutUs';
 import NotFound from './pages/NotFound';
+import Form from './pages/Form';
 import HeaderComponent from './components/header/HeaderComponent';
 
 class App extends Component {
@@ -14,6 +15,7 @@ class App extends Component {
         <Routes>
           <Route path="/" element={<Main />} />
           <Route path="/about" element={<AboutUs />} />
+          <Route path="/form" element={<Form />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </>
