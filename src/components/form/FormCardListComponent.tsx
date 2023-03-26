96df79148871986a0c929dda1fbc366438af9dc8
Aliaksandr Bulynka <abulynka@gmail.com>
2023-03-26 16:42:30 +0200

    feat: add form card list component

---
+++ src/components/form/FormCardListComponent.tsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import FormItem from '../../data/FormItem';
+import FormCardComponent from './FormCardComponent';
+import './FormCardListComponent.css';
+
+class FormCardListComponent extends Component<{ formItems: FormItem[] }> {
+  public constructor(props: { formItems: FormItem[] }) {
+    super(props);
+    this.state = {
+      formItems: props.formItems,
+    };
+  }
+
+  public render() {
+    return (
+      <div className="form-card-list">
+        {this.props.formItems.map((formItem: FormItem) => (
+          <FormCardComponent key={crypto.randomUUID()} formItem={formItem}></FormCardComponent>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default FormCardListComponent;
