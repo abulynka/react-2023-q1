c1d02c395db15ad5cf5b14cfb669cdf0e6c2e3cd
Aliaksandr Bulynka <abulynka@gmail.com>
2023-03-26 16:41:20 +0200

    feat: add item interface

---
+++ src/data/FormItem.tsx
@@ -0,0 +1,15 @@
+interface FormItem {
+  name: string;
+
+  bestBeforeDate: string;
+
+  deliveryBy: string;
+
+  cook: boolean;
+
+  shouldBePacked: boolean;
+
+  file: string;
+}
+
+export default FormItem;
